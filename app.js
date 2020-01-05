90af54f4ed14fee2015b9d70f0346b0050ab3c9d
Harry Gothold <harrygot@hotmail.com>
2020-01-05 13:06:51 +0000

    some types, functions

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
-// Union types
-function combine(input1, input2, resultConversion) {
-    var result;
-    if ((typeof input1 === "number" && typeof input2 === "number") ||
-        resultConversion === "as-number") {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
+function generateError(message, code) {
+    throw {
+        message: message,
+        errorCode: code
+    };
 }
-var combinedAges = combine(30, 26, "as-number");
-console.log(combinedAges);
-var combinedStringAges = combine("30", "26", "as-number");
-console.log(combinedStringAges);
-var combinedNames = combine("Harry", "Joseph", "as-text");
-console.log(combinedNames);
+generateError("Error occured", 500);
